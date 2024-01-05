7b3ffabcddefe7350c2760daefe29203dc5000a2
Christian Feichtinger <ch.fei@hotmail.com>
2024-01-05 16:39:56 +0100

    Update dataset.py
    Dataset-Loading Update

--- code/dataset.py
+++ code/dataset.py
@@ -9,10 +9,14 @@ def toTensor(inp):
     return torch.from_numpy(np.moveaxis(inp, -1, -3)).contiguous().float()
 
 class FocalDataset(torch.utils.data.Dataset):
-    def __init__(self, path='./integrals', input_channels=3, output_channels=3, augment=False, normalize=True, seed=42):
+    def __init__(self, path='./integrals', used_focal_lengths_idx=None,
+                 input_channels=3, output_channels=3, augment=False, normalize=True, seed=42):
         '''
         Dataset class to load generated integral focal stacks
-        
+
+        used_focal_lengths_idx: list with idx-values from 0-9 are allowed, O = focal-length with 0m, 1 = focal-length with 0.2m,
+            2 = 0.4m, 3 = 0.6m, 4 = 0.8m, ...
+
         path: path to directory filled with integral focal stacks and ground truths
         grayscale: if True, return integrals and ground truths with a single color channel. If False, repeats that single channel three times for "rbg"
         augment: randomly flip and rotate
@@ -26,16 +30,23 @@ class FocalDataset(torch.utils.data.Dataset):
         self.augment = augment
         self.normalize = normalize
         self.rng = np.random.default_rng(seed)
+
+        if used_focal_lengths_idx is None:
+            self.used_focal_lengths_idx = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        else:
+            self.used_focal_lengths_idx = used_focal_lengths_idx
         
     def __len__(self):
         return self.num_files
-    
+
     def __getitem__(self, index):
         ok, focal_stack = cv2.imreadmulti(os.path.join(self.path, f'{index}_integral.tiff'))
         if not ok:
             raise IOError(f'Failed to load index: {index}')
             
         focal_stack = np.stack(focal_stack)[...,None]   # shape (num_focal_lengths, w, h, 1)
+        focal_stack = focal_stack[self.used_focal_lengths_idx]
+
         ground_truth = cv2.imread(os.path.join(self.path, f'{index}_gt.png'))[...,[0]]   # shape (w, h, 1)
         
         if self.augment:
